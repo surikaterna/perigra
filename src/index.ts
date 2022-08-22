ecdc8c446d5688632019742e9a74b029700c29ab
heartmon <>
2022-08-22 16:05:20 +0200

    export classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export * from './Graph';
+export * from './Node';
+export * from './Entity';
+export * from './Path';
+export * from './EntityId';
+export * from './EntityType';
+export * from './update/GraphUpdater'
