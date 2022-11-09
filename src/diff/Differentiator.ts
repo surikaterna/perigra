d53baf63cb1e222d6df43d12aec9f9a702dc2d47
heartmon <>
2022-11-09 18:09:19 +0100

    fix type

--- src/diff/Differentiator.ts
+++ src/diff/Differentiator.ts
@@ -1,12 +1,14 @@
+import Entity from '../Entity';
 import EntityId from '../EntityId';
 import Graph from '../Graph';
+import Relation from '../Relation';
 import GraphAction, { ActionType } from '../update/GraphAction';
 
 /**
  * Resolves the difference between two graphs
  */
 export default class Differentiator<N, P> {
-  difference(base: Graph<N, P>, head: Graph<N, P>): readonly GraphAction<N | P>[] {
+  difference(base: Graph<N, P>, head: Graph<N, P>): readonly GraphAction<N | P | Relation<P, N>>[] {
     // Unique "set" of id's from both graphs (head and base)
     const ids = Array.from(new Set(Array.from(base.entityIds()).concat(Array.from(head.entityIds()))));
     const changes = [];
@@ -19,7 +21,7 @@ export default class Differentiator<N, P> {
     return changes;
   }
 
-  private checkEntityDifference(base: Graph<N, P>, head: Graph<N, P>, id: EntityId): GraphAction<N | P> | undefined {
+  private checkEntityDifference(base: Graph<N, P>, head: Graph<N, P>, id: EntityId): GraphAction<N | P | Relation<P, N>> | undefined {
     const b = base.getEntityUnsafe(id);
     const h = head.getEntityUnsafe(id);
     // No change
